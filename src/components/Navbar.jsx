a0edbfcb0ccee7f753e62a0fc040d05882913440
Aldrin Rosales <104087976+Codedrin@users.noreply.github.com>
2025-11-13 11:40:31 +0800

    Update CV download link in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         </NavLink>
      {/* CV Download Button */}
         <a
-          href="https://drive.google.com/file/d/14rId-I9p6KiFZPHijpWSy5M8tQD1_NSV/view?usp=sharing" // Replace with the correct path to your CV file
+          href="https://drive.google.com/file/d/1SE474mIA5dQh_AuK2BVngxotskn3CO8D/view?usp=drivesdk" // Replace with the correct path to your CV file
           download="Aldrin Rosales.pdf" // This sets the file name for the download
         className="blue-gradient_text" 
         >
